9a404fc076d48e4cf39800e72265db7cb4cd0565
Radhe-Radhe19 <swatitodwal@gmail.com>
2025-04-01 13:09:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
 import Header from "./components/header/Header";
-import ProductDetails from "./pages/productDetails/Productdetails";
+import ProductDetails from "./pages/productDetails/ProductDetails";
 
 function App() {
 
